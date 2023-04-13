d08d3968cde1582c16c7cd28ba17d1c73d7663dd
“gsnorberto” <sn_gabriel@outlook.com>
2023-04-12 21:32:43 -0300

    feat: Repository changed to prisma

--- src/repository/carRepository.ts
+++ src/repository/carRepository.ts
@@ -1,30 +1,43 @@
 import db from "../config/database.js";
+import prisma from "../config/database.js";
 
 async function getCars() {
-  const data = await db.query(`SELECT * FROM cars`);
-  return data.rows;
+  return await prisma.cars.findMany()
 }
 
 async function getCar(id: number) {
-  const data = await db.query(`SELECT * FROM cars WHERE id = $1`, [id]);
-  return data.rows[0];
+  return await prisma.cars.findUnique({
+    where: {
+      id
+    }
+  })
 }
 
 async function getCarWithLicensePlate(licensePlate: string) {
-  const data = await db.query(`SELECT * FROM cars WHERE "licensePlate" = $1`, [licensePlate]);
-  return data.rows[0];
+  return await prisma.cars.findUnique({
+    where: {
+      licensePlate
+    }
+  })
 }
 
 async function createCar(model: string, licensePlate: string, year: number, color: string) {
-  await db.query(
-    `INSERT INTO cars (model, "licensePlate", year, color)
-     VALUES ($1, $2, $3, $4)`,
-    [model, licensePlate, year, color]
-  );
+  return await prisma.cars.create({
+    data: {
+      model,
+      licensePlate,
+      year,
+      color
+    }
+  })
 }
 
 async function deleteCar(id: number) {
-  await db.query(`DELETE FROM cars WHERE id = $1`, [id]);
+  return await prisma.cars.delete({
+    where: {
+      id
+    }
+  })
 }
 
 const carRepository = {
